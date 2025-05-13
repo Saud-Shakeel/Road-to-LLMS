8fc552b2c7ff1fa549e908b96a0810872ef56510
Muhammad Saud Shakeel <i.saudshakeel@gmail.com>
2025-05-13 16:34:46 +0500

    made some updates

--- week3/optimized.cpp
+++ week3/optimized.cpp
@@ -5,10 +5,10 @@
 double calculate(long long iterations, double param1, double param2) {
     double result = 1.0;
     for (long long i = 1; i <= iterations; ++i) {
-        double j = i * param1 - param2;
-        result -= (1.0 / j);
-        j = i * param1 + param2;
-        result += (1.0 / j);
+        double j1 = i * param1 - param2;
+        result -= (1.0 / j1);
+        double j2 = i * param1 + param2;
+        result += (1.0 / j2);
     }
     return result;
 }
@@ -18,8 +18,10 @@ int main() {
     double result = calculate(100000000, 4.0, 1.0) * 4.0;
     auto end_time = std::chrono::high_resolution_clock::now();
 
-    std::cout << std::fixed << std::setprecision(12) << "Result: " << result << std::endl;
     std::chrono::duration<double> execution_time = end_time - start_time;
+
+    std::cout << std::fixed << std::setprecision(12);
+    std::cout << "Result: " << result << std::endl;
     std::cout << "Execution Time: " << execution_time.count() << " seconds" << std::endl;
 
     return 0;
